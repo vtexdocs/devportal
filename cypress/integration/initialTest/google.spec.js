ee61083bb9d7ce6489244e92adc5c867c949b343
Melissa Falcão <melissacorreafalcao@gmail.com>
2022-03-21 15:00:31 -0300

    cypress init

---
+++ cypress/integration/initialTest/google.spec.js
@@ -0,0 +1,5 @@
+describe('My First Test', () => {
+  it('Visits Google', () => {
+    cy.visit('http://www.google.com')
+  })
+})
---
+++ cypress/integration/initialTest/visit.spec.js
@@ -0,0 +1,5 @@
+describe('My First Test', () => {
+  it('Visits preview', () => {
+    cy.visit('/')
+  })
+})
