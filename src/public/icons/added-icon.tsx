95c4d22370822d9faadab657314ff4d240b1fb6e
Leonardo Anjos <las4@cin.ufpe.br>
2022-03-21 19:16:34 -0300

    Fix: change kebab-case props to camelCase format in icons

--- src/public/icons/added-icon.tsx
+++ src/public/icons/added-icon.tsx
@@ -8,11 +8,11 @@ import { Icon } from '@vtex/brand-ui'
 const AddedIcon = (props: IconProps) => (
   <Icon {...props} viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="8" cy="8.99805" r="8" fill="#E6F2E6"/>
-    <g clip-path="url(#clip0_2354_76889)">
-      <path d="M4.3335 8.99805H11.6668" stroke="#79A479" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M8.00049 5.33203V12.6654" stroke="#79A479" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M3.93359 8.99831C3.93359 8.77739 4.11268 8.59831 4.33359 8.59831H11.6669C11.8878 8.59831 12.0669 8.77739 12.0669 8.99831C12.0669 9.21922 11.8878 9.39831 11.6669 9.39831H4.33359C4.11268 9.39831 3.93359 9.21922 3.93359 8.99831Z" fill="#79A479"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M8.00026 4.93164C8.22117 4.93164 8.40026 5.11073 8.40026 5.33164V12.665C8.40026 12.8859 8.22117 13.065 8.00026 13.065C7.77935 13.065 7.60026 12.8859 7.60026 12.665V5.33164C7.60026 5.11073 7.77935 4.93164 8.00026 4.93164Z" fill="#79A479"/>
+    <g clipPath="url(#clip0_2354_76889)">
+      <path d="M4.3335 8.99805H11.6668" stroke="#79A479" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M8.00049 5.33203V12.6654" stroke="#79A479" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path fillRule="evenodd" clipRule="evenodd" d="M3.93359 8.99831C3.93359 8.77739 4.11268 8.59831 4.33359 8.59831H11.6669C11.8878 8.59831 12.0669 8.77739 12.0669 8.99831C12.0669 9.21922 11.8878 9.39831 11.6669 9.39831H4.33359C4.11268 9.39831 3.93359 9.21922 3.93359 8.99831Z" fill="#79A479"/>
+      <path fillRule="evenodd" clipRule="evenodd" d="M8.00026 4.93164C8.22117 4.93164 8.40026 5.11073 8.40026 5.33164V12.665C8.40026 12.8859 8.22117 13.065 8.00026 13.065C7.77935 13.065 7.60026 12.8859 7.60026 12.665V5.33164C7.60026 5.11073 7.77935 4.93164 8.00026 4.93164Z" fill="#79A479"/>
     </g>
     <defs>
       <clipPath id="clip0_2354_76889">
--- src/public/icons/vtex-io-icon.tsx
+++ src/public/icons/vtex-io-icon.tsx
@@ -7,9 +7,9 @@ import { Icon } from '@vtex/brand-ui'
 
 const VTEXIOIcon = (props: IconProps) => (
   <Icon {...props} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M4 9.36533L15.9827 14.732L28 9.36533L16.0173 4L4 9.36533Z" stroke="#4A596B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M20.5813 19.312L28 22.6347L15.9827 28L4 22.6347L11.4187 19.3213" stroke="#4A596B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M20.5813 12.6787L28 16L15.9827 21.3654L4 16L11.4187 12.688" stroke="#4A596B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path fillRule="evenodd" clipRule="evenodd" d="M4 9.36533L15.9827 14.732L28 9.36533L16.0173 4L4 9.36533Z" stroke="#4A596B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M20.5813 19.312L28 22.6347L15.9827 28L4 22.6347L11.4187 19.3213" stroke="#4A596B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M20.5813 12.6787L28 16L15.9827 21.3654L4 16L11.4187 12.688" stroke="#4A596B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </Icon>
 )
 
