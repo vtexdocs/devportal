e4745916141f37754d29e38fae4e257ab1a6650a
Robson Oliveira <ropj@cin.ufpe.br>
2022-03-23 11:59:44 -0300

    refactor: change newsletter style name

--- src/components/newsletter-section/index.tsx
+++ src/components/newsletter-section/index.tsx
@@ -2,36 +2,36 @@ import { Box, Button, Flex, Input, Link, Text } from '@vtex/brand-ui'
 
 import Image from 'next/image'
 import landingProduct from 'public/images/landing-product.png'
-import styles2 from 'components/newsletter-section/style'
-import styles from './index.module.css'
+import styles from 'components/newsletter-section/style'
+import imgStyle from './index.module.css'
 
 const NewsletterSection = () => {
   return (
-    <Box sx={styles2.newsletterContainer}>
-      <Flex sx={styles2.newsletterBackground}>
-        <Flex sx={styles2.newsletterBox}>
-          <Text sx={styles2.newsletterTitle}>
+    <Box sx={styles.newsletterContainer}>
+      <Flex sx={styles.newsletterBackground}>
+        <Flex sx={styles.newsletterBox}>
+          <Text sx={styles.newsletterTitle}>
             Build first-class commerce experiences with VTEX.
           </Text>
-          <Text sx={styles2.newsletterDescription}>
+          <Text sx={styles.newsletterDescription}>
             Subscribe to our developer newsletter and stay on top of the
             practices and technologies that are transforming commerce.
           </Text>
-          <Flex sx={styles2.newsletterInputBox}>
+          <Flex sx={styles.newsletterInputBox}>
             <Input
-              sx={styles2.newsletterInput}
+              sx={styles.newsletterInput}
               id="newsletter-mail"
               label="E-mail"
               type={'email'}
             ></Input>
             <Button variant="primary">Subscribe</Button>
           </Flex>
-          <Text sx={styles2.policyText}>
-            Read our <Link sx={styles2.policyLink}>privacy policy</Link>
+          <Text sx={styles.policyText}>
+            Read our <Link sx={styles.policyLink}>privacy policy</Link>
           </Text>
         </Flex>
       </Flex>
-      <div className={styles.wrapper}>
+      <div className={imgStyle.wrapper}>
         <Image src={landingProduct} />
       </div>
     </Box>
