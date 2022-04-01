2d6264b3e45f9360f532adc2b07da684bc6da42b
Leonardo Anjos <las4@cin.ufpe.br>
2022-04-01 16:12:16 -0300

    feat(api-reference): add default rendering of Catalog and Checkout APIs

---
+++ src/pages/docs/api-reference/[slug].tsx
@@ -0,0 +1,58 @@
+import Script from 'next/script'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+
+interface Props {
+  slug: string
+}
+
+const APIPage: NextPage<Props> = ({ slug }) => {
+  return (
+    <>
+      <Script
+        src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"
+        strategy="beforeInteractive"
+      />
+
+      <rapi-doc
+        spec-url={`/docs/${slug}.json`}
+        theme="light"
+        show-header={false}
+        render-style="focused"
+        nav-bg-color="#FFFFFF"
+        primary-color="#142032"
+        regular-font="VTEX Trust Variable"
+        style={{ height: '100%', width: '100%', marginTop: '5rem' }}
+      />
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const paths = [
+    {
+      params: {
+        slug: 'catalog',
+      },
+    },
+    {
+      params: {
+        slug: 'checkout',
+      },
+    },
+  ]
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = ({ params }) => {
+  return {
+    props: {
+      slug: params?.slug,
+    },
+  }
+}
+
+export default APIPage
--- src/pages/docs/api-reference/index.tsx
+++ src/pages/docs/api-reference/index.tsx
@@ -1,5 +1,25 @@
-const ApiReferencePage = () => {
-  return <h2>API-Reference</h2>
+import Link from 'next/link'
+import { Box } from '@vtex/brand-ui'
+
+import styles from 'styles/api-reference'
+
+const APIReferencePage = () => {
+  return (
+    <Box sx={styles.container}>
+      <ul>
+        <li>
+          <Link href="/docs/api-reference/catalog">
+            <a>Catalog API</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/docs/api-reference/checkout">
+            <a>Checkout API</a>
+          </Link>
+        </li>
+      </ul>
+    </Box>
+  )
 }
 
-export default ApiReferencePage
+export default APIReferencePage
