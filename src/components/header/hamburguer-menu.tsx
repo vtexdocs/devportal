246d6120bde776d63500cdd37f9c1992ca74ad33
Melissa Falcão <melissacorreafalcao@gmail.com>
2022-04-12 17:28:15 -0300

    fix(hamburguer-menu): change name component and file

---
+++ src/components/header/hamburguer-menu.tsx
@@ -0,0 +1,11 @@
+import { Header, HamburgerMenu as VtexHamburguerMenu } from '@vtex/brand-ui'
+
+const MenuHamburger = () => {
+  return (
+    <Header.ActionButton>
+      <VtexHamburguerMenu />
+    </Header.ActionButton>
+  )
+}
+
+export default MenuHamburger
