d26d4c123b183cabc1e1b5b8e625f941038ddde2
Robson Oliveira <ropj@cin.ufpe.br>
2022-04-13 14:59:05 -0300

    style(search-page): fix the divider style

--- src/styles/search-page.ts
+++ src/styles/search-page.ts
@@ -8,10 +8,18 @@ const body: SxStyleProp = {
 const resultContainer: SxStyleProp = {
   width: '720px',
   paddingTop: '64px',
+  hr: {
+    marginTop: '16px',
+    marginBottom: '32px',
+    borderTop: 'none',
+    borderColor: '#DDDDDD',
+  },
 }
 
 const resultText: SxStyleProp = {
   mb: '16px',
+  fontSize: '16px',
+  lineHeight: '22px',
 }
 
 export default {
