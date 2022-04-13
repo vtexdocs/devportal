cdbe7c2ac974b02825a208be9e8bcc92f1d45864
Melissa Falcão <melissacorreafalcao@gmail.com>
2022-04-13 12:06:20 -0300

    fix(hamburger-menu): fix the portuguese name

---
+++ src/components/header/hamburger-menu.tsx
@@ -0,0 +1,11 @@
+import { Header, HamburgerMenu as VtexHamburgerMenu } from '@vtex/brand-ui'
+
+const HamburgerMenu = () => {
+  return (
+    <Header.ActionButton>
+      <VtexHamburgerMenu />
+    </Header.ActionButton>
+  )
+}
+
+export default HamburgerMenu
