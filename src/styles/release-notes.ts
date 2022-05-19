3f973cd8ece31a2e8b05d9b02a10de85b7b2a8ff
Melissa Falcão <melissacorreafalcao@gmail.com>
2022-05-19 08:24:28 -0300

    feat(styles): create release styles

--- src/styles/release-notes.ts
+++ src/styles/release-notes.ts
@@ -1,7 +1,10 @@
-import type { SxStyleProp } from '@vtex/brand-ui'
+import { SxStyleProp } from '@vtex/brand-ui'
 
 const container: SxStyleProp = {
   pt: '5rem',
+  background: '#FFFFFF',
 }
 
-export default { container }
+export default {
+  container,
+}
