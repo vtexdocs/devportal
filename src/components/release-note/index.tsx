edd2c2ee5af0af76bec57fd0fbb3d90f6094f59d
Melissa Falcão <melissacorreafalcao@gmail.com>
2022-05-30 19:01:55 -0300

    feat(release-toggle): create toggle for open and close the release description

--- src/components/release-note/index.tsx
+++ src/components/release-note/index.tsx
@@ -1,9 +1,11 @@
-import { Flex, Timeline, Text, Box } from '@vtex/brand-ui'
+import React, { useContext } from 'react'
+import { Flex, Timeline, Text, Box, Button, IconCaret } from '@vtex/brand-ui'
 
 import type { ReleaseElement } from 'utils/typings/types'
 import type { ActionType } from 'components/last-updates-card/functions'
 import { getAction } from './../last-updates-card/functions'
 import { getDaysElapsed } from './../../utils/get-days-elapsed'
+import { Context } from 'utils/contexts/context'
 import styles from './styles'
 
 export interface ReleaseProps {
@@ -13,6 +15,20 @@ export interface ReleaseProps {
   actionType?: ActionType
 }
 
+interface DescriptionProps {
+  description: string
+  releaseStatus?: boolean
+  first?: boolean
+}
+
+const Description = ({ description, releaseStatus }: DescriptionProps) => {
+  if (releaseStatus) {
+    return <Text sx={styles.releaseDescription}>{description}</Text>
+  } else {
+    return <></>
+  }
+}
+
 const ReleaseNote = ({
   title,
   date,
@@ -20,8 +36,39 @@ const ReleaseNote = ({
   actionType,
 }: ReleaseElement) => {
   const actionValue = actionType ? getAction(actionType) : null
+  const { releaseElementStatus, toggleReleaseElementStatus } =
+    useContext(Context)
   return (
-    <Box sx={styles.releaseContainer}>
+    <Flex sx={styles.releaseContainer}>
+      <Button
+        size="regular"
+        variant="tertiary"
+        sx={
+          releaseElementStatus.has(title) &&
+          releaseElementStatus.get(title)?.open
+            ? styles.arrowIconActive
+            : styles.arrowIcon
+        }
+        icon={() => (
+          <IconCaret
+            direction={
+              releaseElementStatus.has(title) &&
+              releaseElementStatus.get(title)?.open
+                ? 'down'
+                : 'right'
+            }
+            size={24}
+          />
+        )}
+        onClick={() => {
+          releaseElementStatus.has(title)
+            ? toggleReleaseElementStatus(
+                title,
+                !releaseElementStatus.get(title)?.open
+              )
+            : toggleReleaseElementStatus(title, true)
+        }}
+      />
       <Timeline.Event
         title={<Text sx={styles.actionType}>{actionValue?.title}</Text>}
         icon={
@@ -37,10 +84,13 @@ const ReleaseNote = ({
           <Text sx={styles.releaseDate}>{`${getDaysElapsed(
             date
           )} days ago`}</Text>
-          <Text sx={styles.releaseDescription}>{description}</Text>
+          <Description
+            description={description}
+            releaseStatus={releaseElementStatus.get(title)?.open}
+          ></Description>
         </Flex>
       </Timeline.Event>
-    </Box>
+    </Flex>
   )
 }
 
