cd9d091a2cc64ab419feda0d6ab89bd68c30d64f
Josué Fidelis <jfon@cin.ufpe.br>
2022-06-03 20:43:15 -0300

    fix(see-also-section): adjust section margin and title font-weight

--- src/components/see-also-section/styles.ts
+++ src/components/see-also-section/styles.ts
@@ -1,7 +1,6 @@
 import { SxStyleProp } from '@vtex/brand-ui'
 
 const seeAlsoContainer: SxStyleProp = {
-  marginLeft: '20px',
   maxWidth: ['324px', '544px', '544px', '544px', '544px', '720px'],
 }
 
@@ -9,6 +8,7 @@ const sectionTitle: SxStyleProp = {
   fontSize: ['18px', '22px'],
   lineHeight: ['30px', '32px'],
   marginBottom: ['16px', '24px'],
+  fontWeight: '400',
   color: '#4A4A4A',
 }
 
