a63d1b266851ec01b4132e91343a84ab837558c1
Leonardo Anjos <las4@cin.ufpe.br>
2022-06-09 18:00:49 -0300

    feat: add string utils

---
+++ src/utils/string-utils.ts
@@ -0,0 +1,19 @@
+export const removeHTML = (str: string) => str.replace(/<\/?[a-z]+>/g, '')
+
+export const slugify = (str: string) => {
+  return str
+    .toLowerCase()
+    .replace(' ', '-')
+    .replace(/[^a-z\-]/g, '')
+}
+
+type Child = string | { props: { children: Child[] } }
+
+export const childrenToString: (children: Child[]) => string = (children) => {
+  return children
+    .map((child) => {
+      if (typeof child === 'string') return child
+      return childrenToString(child.props.children)
+    })
+    .join('')
+}
