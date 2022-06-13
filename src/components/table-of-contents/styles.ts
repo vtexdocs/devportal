006dfb311b0eb52b7498e65db3c0eb2e48d2b893
Leonardo Anjos <las4@cin.ufpe.br>
2022-06-13 15:05:09 -0300

    fix(table-of-contents): fix style bugs and add responsivity

--- src/components/table-of-contents/styles.ts
+++ src/components/table-of-contents/styles.ts
@@ -14,9 +14,9 @@ const item: (level: number, active: boolean) => SxStyleProp = (
   py: '4px',
   ml: '-1px',
   pl: `${level * 8}px`,
-  borderLeft: `1px solid ${active ? '#E31C58' : '#E7E9EE'}`,
-  fontSize: '12px',
-  lineHeight: '16px',
+  borderLeft: `1px solid ${active && level === 1 ? '#E31C58' : '#E7E9EE'}`,
+  fontSize: [0, 0, 0, 0, '12px', '16px'],
+  lineHeight: [0, 0, 0, 0, '16px', '18px'],
   fontWeight: `${active ? '600' : '400'}`,
   color: `${active ? '#0C1522' : 'muted.0'}`,
 
