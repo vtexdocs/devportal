b20506ab078437c17253442fc90f2fba7fbe18b7
Robson Oliveira <ropj@cin.ufpe.br>
2022-06-14 09:23:13 -0300

    fix(feedback-section): fix the dislike button margin

--- src/components/feedback-section/functions.ts
+++ src/components/feedback-section/functions.ts
@@ -2,16 +2,15 @@ import { SxStyleProp } from '@vtex/brand-ui'
 import styles from './styles'
 
 export const setButtonStyle = (
-  feedbackSelected: boolean | undefined,
+  feedback: boolean | undefined,
   like: boolean
 ): SxStyleProp => {
   const ml = like
     ? ['0', '8px']
-    : ['32px', `${feedbackSelected === like ? '8px' : '16px'}`]
-
-  if (feedbackSelected === undefined)
-    return { ...styles.box, ...styles.button, ml }
-  if (like === feedbackSelected)
-    return { ...styles.box, ...styles.selectedButton, ml }
+    : feedback === like
+    ? ['0', '8px']
+    : ['32px', '16px']
+  if (feedback === undefined) return { ...styles.box, ...styles.button, ml }
+  if (like === feedback) return { ...styles.box, ...styles.selectedButton, ml }
   return { display: 'none !important' }
 }
