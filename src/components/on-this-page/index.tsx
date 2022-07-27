a8b2f7e8232cd0b6d5de78abe7535aa1119e19d4
Leonardo Anjos <las4@cin.ufpe.br>
2022-07-26 21:26:13 -0300

    fix(on-this-page): remove contributors from component

--- src/components/on-this-page/index.tsx
+++ src/components/on-this-page/index.tsx
@@ -1,58 +1,21 @@
 import { useState } from 'react'
-import { Box, Flex, Grid, IconCaret, Text } from '@vtex/brand-ui'
+import { Box, Flex, Text } from '@vtex/brand-ui'
 
 import MenuIcon from 'components/icons/menu-icon'
 import CloseIcon from 'components/icons/close-icon'
 
-import Tooltip from 'components/tooltip'
-
 import styles from './styles'
 import TableOfContents from 'components/table-of-contents'
 
-interface Props {
-  contributors: string[]
-}
-
-const OnThisPage = ({ contributors }: Props) => {
+const OnThisPage = () => {
   const [open, setOpen] = useState(false)
-  const [showContributors, setShowContributors] = useState(false)
 
   return (
     <Flex sx={styles.container}>
       {open && (
         <Box sx={styles.contentContainer}>
-          <Flex sx={styles.contributorsHeaderContainer}>
-            <Flex sx={styles.contributorsTitleContainer}>
-              <Text sx={styles.contributorsTitle}>
-                {showContributors ? 'Hide contributors' : 'Show contributors'}
-              </Text>
-              <Text sx={styles.contributorsCount}>{contributors.length}</Text>
-            </Flex>
-
-            <IconCaret
-              size={24}
-              direction={showContributors ? 'up' : 'down'}
-              sx={styles.contributorsCaret}
-              onClick={() =>
-                setShowContributors((showContributors) => !showContributors)
-              }
-            />
-          </Flex>
-
-          {showContributors && (
-            <Grid sx={styles.photoGrid}>
-              {contributors.map((contributor) => (
-                <a key={contributor} href="#">
-                  <Tooltip label={contributor}>
-                    <Box sx={styles.photo} />
-                  </Tooltip>
-                </a>
-              ))}
-            </Grid>
-          )}
-
           <Text sx={styles.onThisPageTitle}>On this page</Text>
-          <Box sx={styles.tableOfContents}>
+          <Box>
             <TableOfContents />
           </Box>
         </Box>
@@ -68,7 +31,6 @@ const OnThisPage = ({ contributors }: Props) => {
         {!open && (
           <Box sx={styles.titlesContainer}>
             <Text sx={styles.title}>On this page</Text>
-            <Text sx={styles.title}>Contributors</Text>
           </Box>
         )}
       </Flex>
--- src/components/on-this-page/styles.ts
+++ src/components/on-this-page/styles.ts
@@ -8,79 +8,20 @@ const container: SxStyleProp = {
 
 const contentContainer: SxStyleProp = {
   mr: '8px',
-  py: '16px',
   width: '360px',
+  padding: '16px',
   borderRadius: '8px',
   backgroundColor: 'white',
   border: '1px solid #CCCED8',
   boxShadow: '0px 0px 16px rgba(0, 0, 0, 0.1)',
 }
 
-const contributorsHeaderContainer: SxStyleProp = {
-  px: '16px',
-  mb: '16px',
-  justifyContent: 'space-between',
-}
-
-const contributorsTitleContainer: SxStyleProp = {
-  alignItems: 'center',
-}
-
-const contributorsTitle: SxStyleProp = {
-  fontSize: '16px',
-  fontWeight: '400',
-  lineHeight: '22px',
-  color: '#0C1522',
-}
-
-const contributorsCount: SxStyleProp = {
-  ml: '4px',
-  px: '8px',
-  width: '30px',
-  height: '16px',
-  borderRadius: '24px',
-  backgroundColor: 'muted.4',
-  fontSize: '12px',
-  fontWeight: '400',
-  lineHeight: '16px',
-  textAlign: 'center',
-  color: '#4A4A4A',
-}
-
-const contributorsCaret: SxStyleProp = {
-  cursor: 'pointer',
-  borderRadius: '16px',
-  backgroundColor: 'muted.4',
-}
-
-const photoGrid: SxStyleProp = {
-  padding: '16px',
-  gap: '16px 24px',
-  backgroundColor: 'muted.4',
-  gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
-  borderTop: '1px solid #E7E9EE',
-}
-
-const photo: SxStyleProp = {
-  width: '44px',
-  height: '44px',
-  borderRadius: '100%',
-  backgroundColor: 'muted.2',
-}
-
 const onThisPageTitle: SxStyleProp = {
-  px: '16px',
-  pt: '16px',
   mb: '16px',
   fontSize: '12px',
   fontWeight: '400',
   lineHeight: '16px',
   color: '#4A4A4A',
-  borderTop: '1px solid #E7E9EE',
-}
-
-const tableOfContents: SxStyleProp = {
-  ml: '16px',
 }
 
 const buttonContainer: SxStyleProp = {
@@ -117,15 +58,7 @@ const title: SxStyleProp = {
 export default {
   container,
   contentContainer,
-  contributorsHeaderContainer,
-  contributorsTitleContainer,
-  contributorsTitle,
-  contributorsCount,
-  contributorsCaret,
-  photoGrid,
-  photo,
   onThisPageTitle,
-  tableOfContents,
   buttonContainer,
   iconContainer,
   titlesContainer,
