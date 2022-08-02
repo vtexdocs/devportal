35d5c30fa5bdffd4b697321d4ff5cc7f6edb3dbd
Josué Fidelis <jfon@cin.ufpe.br>
2022-08-01 21:00:41 -0300

    test: update rapidoc test to suit rapidoc-mini's characteristics

--- src/tests/cypress/integration/catalog-API-page.spec.js
+++ src/tests/cypress/integration/catalog-API-page.spec.js
@@ -2,33 +2,12 @@
 
 describe('Catalog API', () => {
   beforeEach(() => {
-    cy.visit('/docs/api-reference/catalog#overview')
+    cy.visit('/docs/api-reference/catalog')
   })
 
-  it('API search', () => {
-    const typedText = 'Create SKU Service Value'
+  it('Check rapidoc-mini', () => {
+    const title = 'Get Product and SKU IDs'
 
-    cy.getRapidocElement('#nav-bar-search').type(`${typedText}{enter}`)
-
-    cy.getRapidocElement('.nav-scroll').children('div').should('have.length', 5)
-
-    cy.getRapidocElement('.nav-scroll')
-      .children('.nav-bar-tag-and-paths')
-      .should('have.length', 1)
-      .children('div')
-      .should('have.length', 2)
-
-    cy.getRapidocElement('.nav-scroll')
-      .find('.nav-bar-tag-and-paths > div')
-      .eq(0)
-      .should('contain', 'SKU Service Value')
-
-    cy.getRapidocElement('.nav-scroll')
-      .find('.nav-bar-tag-and-paths > div')
-      .eq(1)
-      .contains(typedText)
-      .click()
-
-    cy.getRapidocElement('h2').should('contain', `${typedText}`)
+    cy.get('rapi-doc-mini').shadow().find('.title').should('have.text', title)
   })
 })
