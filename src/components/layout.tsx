045764a1eb302d2eac554136e5e1913863b808f1
Bruno Amui <bruno.amui@vtex.com.br>
2022-08-28 00:34:46 -0300

    feat(layout.tsx): adds Layout file
    The layout file integrates shared components for every page

---
+++ src/components/layout.tsx
@@ -0,0 +1,29 @@
+import { Flex, Box } from '@vtex/brand-ui'
+import type { ReactElement } from 'react'
+
+import styles from 'styles/documentation-page'
+import Header from 'components/header'
+import Footer from 'components/footer'
+
+import SidebarContextProvider from 'utils/contexts/sidebar'
+import Sidebar from 'components/sidebar'
+
+interface Props {
+  sidebarfallback: any //eslint-disable-line
+  children: ReactElement
+}
+
+export default function Layout({ children, sidebarfallback }: Props) {
+  return (
+    <>
+      <Header />
+      <Flex sx={styles.container}>
+        <SidebarContextProvider fallback={sidebarfallback}>
+          <Sidebar />
+          <Box sx={styles.mainContainer}>{children}</Box>
+        </SidebarContextProvider>
+      </Flex>
+      <Footer />
+    </>
+  )
+}
