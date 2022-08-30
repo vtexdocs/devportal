b0f6c62d9d85fb3a3f23aa28defba34b1dff9416
Bruno Amui <bruno.amui@vtex.com.br>
2022-08-29 21:36:16 -0300

    refactor(api/navigation): removes top level async call from api handler code

---
+++ src/pages/api/docs.ts
@@ -0,0 +1,5 @@
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function handler(req: any, res: any): void {
+  req = req
+  res.status(200).json({})
+}
--- src/pages/api/navigation.ts
+++ src/pages/api/navigation.ts
@@ -1,11 +1,7 @@
-// import useSWR from 'swr'
-import type { SidebarSectionProps } from 'components/sidebar-section'
 import getNavigation from 'utils/getNavigation'
 
-const sidebarDataMaster: SidebarSectionProps[] = await getNavigation()
-
 //eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function handler(req: any, res: any): void {
+export default async function handler(req: any, res: any) {
   req = req
-  res.status(200).json(sidebarDataMaster)
+  res.status(200).json(await getNavigation())
 }
