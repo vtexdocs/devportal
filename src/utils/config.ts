0af82d8530153312f6f7a4107b7c901b03fbaddf
Bruno Amui <bruno.amui@vtex.com.br>
2022-09-06 22:43:52 -0300

    feat: debug

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 function base64(data: string) {
   const buff = Buffer.from(data, 'base64')
-  return buff.toString('utf-8')
+  return buff.toString('ascii')
 }
 
 const getEnvironmentVariable = (environmentVariable: string): string => {
