90a5ab66d66e1c59b240fc6c253e95c2038901a6
Bruno Amui <bruno.amui@vtex.com.br>
2022-09-16 21:25:28 -0300

    feat: debug

--- getPEM.sh
+++ getPEM.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$NETLIFY" = true ] ; then
     echo 'Build On Netlify, writing PEM File'
-	printenv GITHUB_PRIVATEKEY | base64 -d -o github.pem
+	printenv GITHUB_PRIVATEKEY | base64 -d > github.pem
 fi
