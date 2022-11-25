33d0ba82ab43f4dc647574036b873020f8a414af
Carolina Menezes <carolinamenezes@hotmail.com.br>
2022-11-25 19:26:04 -0300

    fix: typo

--- src/components/overview-card/index.tsx
+++ src/components/overview-card/index.tsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.css'
-import Link from 'next/Link'
+import Link from 'next/link'
 import * as Icons from './icons'
 import type { IconComponent } from 'utils/typings/types'
 
