3e69ab152f18828cbfd4625bf3e9f15ac7777521
Carolina Menezes <carolinamenezes@hotmail.com.br>
2022-11-26 20:26:53 -0300

    feat: add-sfpage

---
+++ src/pages/docs/store-framework-apps/index.tsx
@@ -0,0 +1,12 @@
+import Layout from 'components/category-layout'
+
+const SFAppsPage = () => {
+  return (
+    <Layout>
+      <h1>Store Framework Apps</h1>
+      <p>Store Framework</p>
+    </Layout>
+  )
+}
+
+export default SFAppsPage
