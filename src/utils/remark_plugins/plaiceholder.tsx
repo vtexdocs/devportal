398fc7b870006728744ec3a4d738047e326a3b9e
Bruno Amui <bruno.amui@vtex.com.br>
2022-12-01 17:57:15 -0300

    fix(plaiceholder): adds fallback for everything

--- src/utils/remark_plugins/plaiceholder.tsx
+++ src/utils/remark_plugins/plaiceholder.tsx
@@ -10,35 +10,36 @@ async function transformer(ast: any) {
   visit(ast, 'image', visitor)
 
   function visitor(node: { url: string | Buffer; alt: string }) {
-    console.log(`Checking img:${node.url}`)
+    console.debug(`Checking img:${node.url}`)
     promises.push(
       probe(node.url as string).then((results) => {
-        console.log(`Probed img:${node.url}`)
+        console.debug(`Probed img:${node.url}`)
         const img = results
+        node.alt = JSON.stringify({
+          base64: '  ',
+          img: img,
+        })
         if (img.width < maxIMGSizePx && img.height < maxIMGSizePx) {
           promises.push(
             getPlaiceholder(node.url).then((resultsPlaiceholder) => {
-              console.log(`Plaiceholder done img:${node.url}`)
+              console.debug(`Plaiceholder done img:${node.url}`)
               node.alt = JSON.stringify({
                 base64: resultsPlaiceholder.base64,
                 img: resultsPlaiceholder.img,
               })
-              console.log(node.alt)
+              console.debug(node.alt)
             })
           )
-        } else {
-          node.alt = JSON.stringify({
-            base64: '  ',
-            img: img,
-            alt: node.alt,
-          })
         }
       })
     )
   }
   await Promise.all(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    promises.map((p: any) => p.catch((e: any) => console.log(e)))
+    promises.map((p: any) =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      p.catch((e: any) => console.error(`\x1b[33m${e}\x1b[0m`))
+    )
   )
 }
 
