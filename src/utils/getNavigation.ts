a50af57e1d354f4271df1d6d7a6e39d3ce8d8769
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2022-12-20 19:55:02 -0300

    feat(navigation): get navigation from public directory

--- src/utils/getNavigation.ts
+++ src/utils/getNavigation.ts
@@ -1,7 +1,5 @@
+import navigation from '../../public/navigation.json'
+
 export default async function getNavigation() {
-  const navigationJsonUrl = process.env.navigationJsonUrl
-  const result = await fetch(navigationJsonUrl as string)
-    .then((res) => res.json())
-    .then((res) => res.navbar)
-  return result
+  return navigation.navbar
 }
