887c8aa267b9d2ba14a622346760576b4edea1e9
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2022-12-21 20:00:00 -0300

    fix(navigation): get navigation.json from CDN

--- src/utils/getNavigation.ts
+++ src/utils/getNavigation.ts
@@ -1,5 +1,7 @@
-import navigation from '../../public/navigation.json'
-
 export default async function getNavigation() {
-  return navigation.navbar
+  const navigationJsonUrl = process.env.navigationJsonUrl
+  const result = await fetch(navigationJsonUrl as string)
+    .then((res) => res.json())
+    .then((res) => res.navbar)
+  return result
 }
