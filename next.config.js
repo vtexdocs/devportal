30c5c01643501d422d438c64d5b09bc63f0648bd
Bruno Amui <bruno.amui@vtex.com.br>
2022-12-28 20:27:57 -0300

    feat(redirects): add vtex-rest-api redirects

--- next.config.js
+++ next.config.js
@@ -23,6 +23,15 @@ const nextConfig = {
     navigationJsonUrl:
       'https://vtex-dev-portal-navigation.fra1.cdn.digitaloceanspaces.com/navigation.json',
   },
+  async redirects() {
+    return [
+      {
+        source: '/vtex-rest-api/docs/:slug',
+        destination: '/docs/api-guides/:slug',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = withPlaiceholder(nextConfig)
