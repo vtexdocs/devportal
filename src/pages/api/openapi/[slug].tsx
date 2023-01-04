5f6f6914dd5357cb5e40731bdc8f3dd9666996cc
Bruno Amui <bruno.amui@vtex.com.br>
2023-01-04 20:42:05 -0300

    feat(references): use edge api to get openapis

---
+++ src/pages/api/openapi/[slug].tsx
@@ -0,0 +1,77 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+  runtime: 'experimental-edge',
+}
+
+const referencePaths = objectFlip({
+  'VTEX - Antifraud Provider API': 'antifraud-provider-protocol',
+  'VTEX - CMS API': 'cms-api',
+  'VTEX - Catalog API Seller Portal': 'catalog-api-seller-portal',
+  'VTEX - Catalog API': 'catalog-api',
+  'VTEX - Checkout API': 'checkout-api',
+  'VTEX - Customer Credit API': 'customer-credit-api',
+  'VTEX - GiftCard Hub API': 'giftcard-hub-api',
+  'VTEX - Giftcard API': 'giftcard-api',
+  'VTEX - Giftcard Provider Protocol': 'giftcard-provider-protocol',
+  'VTEX - Headless CMS API': 'headless-cms-api',
+  'VTEX - Intelligent Search API': 'intelligent-search-api',
+  'VTEX - License Manager API': 'license-manager-api',
+  'VTEX - Logistics API': 'logistics-api',
+  'VTEX - Marketplace APIs': 'marketplace-apis',
+  'VTEX - Marketplace Protocol': 'marketplace-protocol',
+  'VTEX - Master Data API - v2': 'master-data-api-v2',
+  'VTEX - MasterData API - v10.2': 'masterdata-api',
+  'VTEX - Message Center API': 'message-center-api',
+  'VTEX - Orders API (PII compliant)': 'orders-api-pii-compliant',
+  'VTEX - Orders API': 'orders-api',
+  'VTEX - Payment Provider Protocol': 'payment-provider-protocol',
+  'VTEX - Payments Gateway API': 'payments-gateway-api',
+  'VTEX - Policies System API': 'policies-system-api',
+  'VTEX - Price Simulations': 'price-simulations',
+  'VTEX - Pricing API': 'pricing-api',
+  'VTEX - Pricing Hub': 'pricing-hub',
+  'VTEX - Profile System': 'profile-system',
+  'VTEX - Promotions & Taxes API': 'promotions-and-taxes-api',
+  'VTEX - Recurrence (v1 - deprecated)': 'recurrence-v1-deprecated',
+  'VTEX - Reviews and Ratings API': 'reviews-and-ratings-api',
+  'VTEX - SKU Bindings API': 'sku-bindings-api',
+  'VTEX - Search API': 'search-api',
+  'VTEX - Session Manager API': 'session-manager-api',
+  'VTEX - Subscriptions API (v2)': 'subscriptions-api-v2',
+  'VTEX - Subscriptions API (v3)': 'subscriptions-api-v3',
+  'VTEX - Tracking': 'tracking',
+  'VTEX - VTEX Do API': 'vtex-do-api',
+  'VTEX - VTEX ID API': 'vtex-id-api',
+})
+
+function objectFlip(obj: { [x: string]: string }) {
+  return Object.keys(obj).reduce((ret, key) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ret[obj[key]] = key
+    return ret
+  }, {})
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function handler(req: any) {
+  const { searchParams } = new URL(req.url)
+  const slug = searchParams.get('slug')
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const path = referencePaths[slug] || ''
+  if (path) {
+    return await fetch(
+      `https://raw.githubusercontent.com/vtex/openapi-schemas/master/${path}.json`
+    )
+  } else {
+    return new Response('', {
+      status: 404,
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+  }
+}
--- src/pages/docs/api-reference/[slug].tsx
+++ src/pages/docs/api-reference/[slug].tsx
@@ -20,7 +20,7 @@ const slugs = Object.keys(await getReferencePaths())
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const APIPage: NextPage<Props> = ({ url }) => {
+const APIPage: NextPage<Props> = ({ slug }) => {
   const router = useRouter()
   const rapidoc = useRef<{ scrollTo: (endpoint: string) => void }>(null)
 
@@ -50,7 +50,7 @@ const APIPage: NextPage<Props> = ({ url }) => {
       />
       <rapi-doc
         ref={rapidoc}
-        spec-url={`${url}`}
+        spec-url={`/api/openapi/${slug}`}
         layout="column"
         render-style="focused"
         show-header="false"
