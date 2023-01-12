913ef166ac51d222831e0fdddb9cb8f6fc3a2048
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2023-01-12 12:42:14 -0300

    fix(see-also): fix see also and documentation card components sizes

--- src/components/documentation-card/styles.ts
+++ src/components/documentation-card/styles.ts
@@ -26,34 +26,21 @@ const titleContainer: SxStyleProp = {
   alignItems: 'center',
 }
 
-const icon: SxStyleProp = {
-  width: ['24px', '24px', '24px', '24px', '24px', '24px', '36px'],
-  height: ['24px', '24px', '24px', '24px', '24px', '24px', '36px'],
-}
-
-const text: SxStyleProp = {
-  fontSize: ['16px', '16px', '16px', '16px', '16px', '16px', '24px'],
-  lineHeight: ['22px', '22px', '22px', '22px', '22px', '22px', '28px'],
-  fontWeight: '400',
-  textTransform: 'none',
-}
-
 const title: SxStyleProp = {
-  ...text,
-  ml: ['8px', '8px', '8px', '8px', '8px', '8px', '12px'],
+  ml: '8px',
   color: 'muted.0',
 }
 
 const description: SxStyleProp = {
-  ...text,
-  ml: ['32px', '32px', '32px', '32px', '32px', '32px', '48px'],
+  ml: '32px',
+  fontSize: '16px',
+  lineHeight: '18px',
   color: 'muted.1',
 }
 
 export default {
   cardContainer,
   description,
-  icon,
   title,
   titleContainer,
 }
