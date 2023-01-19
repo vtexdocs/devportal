013e3547840d40112b1f8aa81f341c27af6d9d0b
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2023-01-19 18:13:49 -0300

    feat(search): add search bar and filter tabs to the first three breakpoints of the search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,20 @@
-import { Flex } from '@vtex/brand-ui'
+import { Box, Flex } from '@vtex/brand-ui'
 import styles from 'styles/search-page'
 import SearchSections from 'components/search-sections'
 import SearchContextProvider from 'utils/contexts/search'
 import SearchResults from 'components/search-results'
+import SearchFilterTabBar from 'components/search-filter-tab-bar'
+import SearchInput from 'components/search-input'
 
 const SearchPage = () => {
   return (
     <SearchContextProvider>
+      <Box>
+        <Flex sx={styles.searchBarContainer}>
+          <SearchInput />
+        </Flex>
+        <SearchFilterTabBar />
+      </Box>
       <Flex sx={styles.body}>
         <SearchSections />
         <SearchResults />
