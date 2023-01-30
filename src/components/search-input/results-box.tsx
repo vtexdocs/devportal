6ff10113dd6d4f8ec5e49af4f7910d25cac39a8e
Robson Oliveira <ropj@cin.ufpe.br>
2023-01-30 18:01:51 -0300

    perf(search-input): remove search with empty query

--- src/components/search-input/results-box.tsx
+++ src/components/search-input/results-box.tsx
@@ -65,7 +65,7 @@ const HitsBox = connectStateResults(({ searchState, searchResults }) => {
 
   return (
     <>
-      {searchStateActive?.query && (
+      {searchStateActive?.query && searchResults && (
         <Box sx={styles.resultsContainer}>
           <Box sx={searchResults.hits.length && styles.resultsBox}>
             {searchResults.hits.map(
