70407eee7c2c1ca7df24a21e16a27b8edb332d8e
Bruno Amui <bruno.amui@vtex.com.br>
2023-02-15 15:04:13 -0300

    feat(layout): add GTM

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -52,8 +52,26 @@ export default function Layout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', 'UA-56275648-4');
+        `}
+        </Script>
+        <Script id="GTM-init">
+          {`
+          <!-- Google Tag Manager -->
+					<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-WGQQ964');</script>
+					<!-- End Google Tag Manager -->
+        `}
+        </Script>
+        <Script id="GTM">
+          {`
+          <!-- Google Tag Manager (noscript) -->
+					<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGQQ964"
+					height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+					<!-- End Google Tag Manager (noscript) -->
         `}
         </Script>
       </div>
