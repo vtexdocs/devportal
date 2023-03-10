5e12ee65eb1c659845a1dbbc48e409290a0edeb2
Alice Oliveira <aoqb@cin.ufpe.br>
2023-03-10 10:35:43 -0300

    fix(lightbox): use clickoutside hook

--- src/components/lightbox/index.tsx
+++ src/components/lightbox/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Flex } from '@vtex/brand-ui'
 import CloseIcon from 'components/icons/close-icon'
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement, useEffect, useRef, useState } from 'react'
+import useClickOutside from 'utils/hooks/useClickOutside'
 import styles from './styles'
 
 interface Props {
@@ -9,17 +10,20 @@ interface Props {
 
 export default function LightBox({ children }: Props) {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const modalRef = useRef<HTMLDivElement>()
 
   useEffect(() => {
     if (isModalOpen) document.body.style.overflow = 'hidden'
     else document.body.style.overflow = 'visible'
   }, [isModalOpen])
 
+  useClickOutside(modalRef, () => setIsModalOpen(false))
+
   return (
     <div>
       {isModalOpen && (
-        <Flex sx={styles.container} onClick={() => setIsModalOpen(false)}>
-          <Box sx={styles.modal}>
+        <Flex sx={styles.container}>
+          <Box sx={styles.modal} ref={modalRef}>
             <Box sx={styles.buttonContainer}>
               <Button
                 sx={styles.closeButton}
