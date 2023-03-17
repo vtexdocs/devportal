b7bd30e68e08894bd38e0e4eb59760122cac24d4
Carolina Menezes <carolinamenezes@hotmail.com.br>
2023-03-17 12:05:09 -0300

    fix(disable-preview): fix value declared but never used

--- src/pages/api/disable-preview.ts
+++ src/pages/api/disable-preview.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.clearPreviewData({})
   res.redirect('/')
 }
