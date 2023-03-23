03579028c5c40bce2f1f11465a1db82f96b0e158
Alice Oliveira <aoqb@cin.ufpe.br>
2023-03-23 09:53:01 -0300

    feat: create encrypted file to store environment variables

---
+++ src/utils/get-variables.ts
@@ -0,0 +1,56 @@
+import crypto from 'crypto'
+import fs from 'fs'
+
+const SECRETS_PATH = 'sheets-secrets'
+let secrets: { [key: string]: string } | undefined = undefined
+
+const getSecrets = () => {
+  if (!secrets) {
+    console.log('leu arquivo')
+    let data = fs.readFileSync(SECRETS_PATH, 'utf-8')
+    data = decrypt(data)
+    try {
+      secrets = JSON.parse(data)
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+const getEncryptionKey = (): string => {
+  const unvalidatedEnvironmentVariable = process.env.ENCRYPTION_KEY
+  if (!unvalidatedEnvironmentVariable) {
+    throw new Error(`Couldn't find environment variable: ENCRYPTION_KEY`)
+  } else {
+    return unvalidatedEnvironmentVariable
+  }
+}
+
+function decrypt(text: string) {
+  const textParts = text.split(':')
+  const iv = Buffer.from(textParts.shift() ?? '', 'hex')
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex')
+  const decipher = crypto.createDecipheriv(
+    'aes-256-cbc',
+    Buffer.from(getEncryptionKey()),
+    iv
+  )
+  let decrypted = decipher.update(encryptedText)
+
+  decrypted = Buffer.concat([decrypted, decipher.final()])
+
+  return decrypted.toString()
+}
+
+export const getVariable = (environmentVariable: string): string => {
+  getSecrets()
+  const secret = secrets?.[environmentVariable]
+
+  if (!secret) {
+    throw new Error(
+      `Couldn't find environment variable: ${environmentVariable}`
+    )
+  }
+
+  return secret
+}
