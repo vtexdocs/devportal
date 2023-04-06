2903d2e85cf9f73a3ac4b49293c99f6fcaf14838
Robson Oliveira <ropj@cin.ufpe.br>
2023-04-06 14:47:28 -0300

    perf(sidebar): remove initial shift

--- src/components/sidebar-section/index.tsx
+++ src/components/sidebar-section/index.tsx
@@ -1,5 +1,5 @@
 import { Flex, Box, Text, Button } from '@vtex/brand-ui'
-import { useContext, useEffect, useMemo, useState } from 'react'
+import { useContext, useMemo, useState } from 'react'
 
 import type { SidebarElement } from 'components/sidebar-elements'
 import type {
@@ -43,10 +43,6 @@ const SidebarSection = ({
     { name: 'DELETE', active: false },
     { name: 'PATCH', active: false },
   ])
-
-  useEffect(() => {
-    if (window.innerWidth < 1920) setSidebarSectionHidden(true)
-  }, [])
 
   const filterStatus =
     methodFilterList.some((methodFilter) => methodFilter.active) ||
