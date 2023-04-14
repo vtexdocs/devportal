8ca05689aac695f3e80f01852cb2742c08ab01ca
Carolina Menezes <carolinamenezes@hotmail.com.br>
2023-04-14 17:59:07 -0300

    fix(slug): test en locale

--- src/pages/docs/guides/[slug].tsx
+++ src/pages/docs/guides/[slug].tsx
@@ -180,7 +180,8 @@ export const getStaticProps: GetStaticProps = async ({
       : 'main'
   const branch = preview ? previewBranch : 'main'
   const slug = params?.slug as string
-  const currentLocale = locale || 'en'
+  const currentLocale = locale ? locale : 'en'
+  console.log(currentLocale)
   const docsPaths =
     process.env.NEXT_PHASE === PHASE_PRODUCTION_BUILD
       ? docsPathsGLOBAL
