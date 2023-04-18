8dc8694db48b9c22abad6b5c1cf55fb902db86c2
Júlia Rabello <77292838+julia-rabello@users.noreply.github.com>
2023-04-18 11:11:30 -0300

    feat(api-index): add api-index page
    add a page with api ref index generator tool

---
+++ src/pages/editor/api-index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Head from 'next/head'
+import { Box, Flex } from '@vtex/brand-ui'
+import JsonEditorIndex from 'components/JsonEditorIndex'
+import { Fragment } from 'react'
+import styles from 'styles/documentation-landing-page'
+import { NextPage } from 'next'
+
+import PageHeader from 'components/page-header'
+import image from '../../../public/images/editor.png'
+
+const APIRefAdminPage: NextPage = () => {
+  const headerDescription =
+    'Enter the `children` array of an API reference navigation JSON below and click Submit to convert it to a markdown index.'
+  const headerTitle = 'API Index Generator'
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleJsonDataChange(changedData: any) {
+    JSON.stringify(changedData)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>API Reference Index Generator</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Flex>
+        <Box sx={styles.mainContainer}>
+          <Fragment>
+            <PageHeader
+              title={headerTitle}
+              description={headerDescription}
+              imageUrl={image}
+              imageAlt="API Reference Index Generator"
+            />
+            <Box sx={styles.contentContainer}>
+              <JsonEditorIndex handleJsonDataChange={handleJsonDataChange} />
+            </Box>
+          </Fragment>
+        </Box>
+      </Flex>
+    </>
+  )
+}
+
+export default APIRefAdminPage
