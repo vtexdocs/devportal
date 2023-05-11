e8dbfc10a12e8edc14b6f35db4d427b200431e50
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2023-05-11 18:18:07 -0300

    feat: add authentication higher order component

---
+++ src/components/auth/index.tsx
@@ -0,0 +1,42 @@
+import { ReactElement } from 'react'
+import { Box, Flex, Button, Text } from '@vtex/brand-ui'
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+import styles from './styles'
+
+interface Props {
+  children: ReactElement[]
+}
+
+const Auth = ({ children }: Props) => {
+  const { status } = useSession()
+  if (status === 'authenticated') {
+    return (
+      <>
+        {children}
+        <Button sx={styles.signOutButton} onClick={() => signOut()}>
+          Sign Out
+        </Button>
+      </>
+    )
+  }
+
+  if (status === 'unauthenticated') {
+    return (
+      <Flex sx={styles.signInOuterContainer}>
+        <Box sx={styles.signInInnerContainer}>
+          <Text sx={styles.signInText}>
+            You need to log in to access this content!
+          </Text>
+          <Button sx={styles.signInButton} onClick={() => signIn('github')}>
+            Sign In
+          </Button>
+        </Box>
+      </Flex>
+    )
+  }
+
+  return <></>
+}
+
+export default Auth
