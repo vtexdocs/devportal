1f004e9271173a9edb53eaaa3040b7b5159ac4ae
Robson Oliveira <ropj@cin.ufpe.br>
2023-05-15 15:34:36 -0300

    test(cypress): simplify the test on TOC in guides pages

--- src/tests/cypress/integration/api-guides.cy.js
+++ src/tests/cypress/integration/api-guides.cy.js
@@ -130,25 +130,23 @@ describe('API guides documentation page', () => {
   })
 
   it('try to click on the last element of table of contents', () => {
-    if (Cypress.$('[data-cy="table-of-contents"]').children().length > 0) {
+    if (Cypress.$('h2').length > 0) {
       cy.get('h2')
         .its('length')
         .then((length) => {
-          cy.get('[data-cy="table-of-contents"]:visible > div').should(
-            'have.length',
-            length
-          )
+          cy.get('[data-cy="table-of-contents"]')
+            .should('not.be.empty')
+            .first()
+            .children()
+            .should('have.length', length)
+            .last()
+            .then((heading) => {
+              const anchor = heading.find('a').attr('href')
+              cy.wrap(anchor.substring(anchor.indexOf('#') + 1)).as('anchor')
+              return cy.wrap(heading)
+            })
+            .click()
         })
-
-      cy.get('[data-cy="table-of-contents"]:visible > div')
-        .last()
-        .then((heading) => {
-          const anchor = heading.find('a').attr('href')
-          cy.wrap(anchor.substring(anchor.indexOf('#') + 1)).as('anchor')
-          return cy.wrap(heading)
-        })
-        .click()
-
       cy.get('@anchor').then((anchor) => {
         cy.get(`[id=${anchor}]`).should('be.visible')
       })
