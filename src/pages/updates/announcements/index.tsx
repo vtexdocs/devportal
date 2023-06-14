edce7ee211648daa024db93e70d816f2176352b1
Carolina Menezes <carolinamenezes@hotmail.com.br>
2023-06-14 17:09:53 -0300

    fix(announcements): comment ReleaseSection - needs all frontmatters to be formatted correctly

--- src/pages/updates/announcements/index.tsx
+++ src/pages/updates/announcements/index.tsx
@@ -1,28 +1,26 @@
-import { Flex } from '@vtex/brand-ui'
-import { GetStaticProps, NextPage } from 'next'
-import { DocumentationTitle, UpdatesTitle } from 'utils/typings/unionTypes'
-import getNavigation from 'utils/getNavigation'
-import ReleaseSection from '../../../components/release-section'
+import { Text, Flex } from '@vtex/brand-ui'
+import { NextPage } from 'next'
+//import { DocumentationTitle, UpdatesTitle } from 'utils/typings/unionTypes'
+//import getNavigation from 'utils/getNavigation'
+//import ReleaseSection from '../../../components/release-section'
 
 import styles from 'styles/release-notes'
-import getReleasesData from 'utils/getReleasesData'
-import { UpdateElement } from 'utils/typings/types'
 import Head from 'next/head'
-import { PreviewContext } from 'utils/contexts/preview'
-import { useContext } from 'react'
+//import { PreviewContext } from 'utils/contexts/preview'
+//import { useContext } from 'react'
 import { useIntl } from 'react-intl'
 
-interface Props {
-  sidebarfallback: any //eslint-disable-line
-  sectionSelected?: DocumentationTitle | UpdatesTitle | ''
-  releasesData: UpdateElement[]
-  branch: string
-}
+// interface Props {
+//   sidebarfallback: any //eslint-disable-line
+//   sectionSelected?: DocumentationTitle | UpdatesTitle | ''
+//   releasesData: UpdateElement[]
+//   branch: string
+// }
 
-const ReleasePage: NextPage<Props> = ({ releasesData, branch }) => {
+const ReleasePage: NextPage = () => {
   const intl = useIntl()
-  const { setBranchPreview } = useContext(PreviewContext)
-  setBranchPreview(branch)
+  //const { setBranchPreview } = useContext(PreviewContext)
+  //setBranchPreview(branch)
   return (
     <>
       <Head>
@@ -40,35 +38,35 @@ const ReleasePage: NextPage<Props> = ({ releasesData, branch }) => {
         />
       </Head>
       <Flex sx={styles.container}>
-        <ReleaseSection releasesData={releasesData} />
+        <Text>WIP</Text>
       </Flex>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async ({
-  preview,
-  locale,
-  previewData,
-}) => {
-  const sidebarfallback = await getNavigation()
-  const sectionSelected = 'Announcements'
-  const previewBranch =
-    preview && JSON.parse(JSON.stringify(previewData)).hasOwnProperty('branch')
-      ? JSON.parse(JSON.stringify(previewData)).branch
-      : 'main'
-  const branch = preview ? previewBranch : 'main'
-  const currentLocale = locale ? locale : 'en'
-  const releasesData = await getReleasesData(branch, currentLocale)
+// export const getStaticProps: GetStaticProps = async ({
+//   preview,
+//   locale,
+//   previewData,
+// }) => {
+//   const sidebarfallback = await getNavigation()
+//   const sectionSelected = 'Announcements'
+//   const previewBranch =
+//     preview && JSON.parse(JSON.stringify(previewData)).hasOwnProperty('branch')
+//       ? JSON.parse(JSON.stringify(previewData)).branch
+//       : 'main'
+//   const branch = preview ? previewBranch : 'main'
+//   const currentLocale = locale ? locale : 'en'
+//   const releasesData = await getReleasesData(branch, currentLocale)
 
-  return {
-    props: {
-      sidebarfallback,
-      sectionSelected,
-      releasesData,
-      branch,
-    },
-  }
-}
+//   return {
+//     props: {
+//       sidebarfallback,
+//       sectionSelected,
+//       releasesData,
+//       branch,
+//     },
+//   }
+// }
 
 export default ReleasePage
