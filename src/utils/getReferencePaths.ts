eece0db1a2478d4c4a594afd562db46cfb84dd27
Bruno Amui <brunoamui@gmail.com>
2023-06-27 11:07:20 -0300

    fix(getreferencepaths): check if path is on root

--- src/utils/getReferencePaths.ts
+++ src/utils/getReferencePaths.ts
@@ -75,7 +75,7 @@ export default async function getReferencePaths() {
   repoTree.tree.map((node: any) => {
     const path = node.path
     const re = /^(?<path>.+\/)*(?<filename>.+)\.(?<filetype>.+)$/
-    if (path.startsWith('')) {
+    if (!path.includes('/')) {
       const match = path.match(re)
       const filename = match?.groups?.filename ? match?.groups?.filename : ''
       const filetype = match?.groups?.filetype ? match?.groups?.filetype : ''
