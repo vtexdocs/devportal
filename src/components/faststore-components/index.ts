ae3ade261fd8cc09c5e9413a680e1a00bcaffe33
Robson Oliveira <ropj@cin.ufpe.br>
2023-06-30 18:07:58 -0300

    fix(faststore-components): remove propsection export

--- src/components/faststore-components/index.ts
+++ src/components/faststore-components/index.ts
@@ -22,7 +22,7 @@ export { default as PriceRangeUsage } from './PriceRange/PriceRangeUsage'
 export { default as ProductShelfUsage } from './ProductShelf/ProductShelfUsage'
 export type { ProductShelfUsageProps } from './ProductShelf/ProductShelfUsage'
 export { default as ProductShelfCarouselUsage } from './ProductShelf/ProductShelfCarouselUsage'
-export * from './PropsSection'
+export { default as PropsSection } from './PropsSection'
 export { default as RatingActionable } from './Rating/RatingActionable'
 export type { RatingActionableProps } from './Rating/RatingActionable'
 export { default as SearchInputUsage } from './Search/SearchInputUsage'
@@ -33,11 +33,6 @@ export { default as SlideOverUsage } from './SlideOver/SlideOverUsage'
 export type { SlideOverUsageProps } from './SlideOver/SlideOverUsage'
 export { default as ToastUsage } from './Toast/ToastUsage'
 export * from './Tokens'
-export {
-  faststoreComponentsFromNodeModules,
-  getComponentPropsFrom,
-  mapComponentFromMdxPath,
-} from './utilities/propsSection'
 export {
   useFormattedPrice,
   usePriceFormatter,
