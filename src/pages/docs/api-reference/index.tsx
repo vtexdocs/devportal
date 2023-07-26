488d92cef6d19bdb4aa813e22ece52bade40320f
Júlia Rabello <77292838+julia-rabello@users.noreply.github.com>
2023-07-26 17:20:18 -0300

    docs: update index.tsx

--- src/pages/docs/api-reference/index.tsx
+++ src/pages/docs/api-reference/index.tsx
@@ -28,10 +28,10 @@ const whatsNextData: WhatsNextDataElement[] = [
     linkTo: '/docs/api-reference/antifraud-provider-protocol',
   },
   {
-    title: 'CMS API',
+    title: 'Legacy CMS Portal API',
     description: 'Update your websites’ internet communication protocol',
     linkTitle: 'See more',
-    linkTo: '/docs/api-reference/cms-api',
+    linkTo: '/docs/api-reference/legacy-cms-portal-api',
   },
   {
     title: 'Catalog API',
