109b2ab464f1e761a56e641d36f57290675cbb29
aliceoq <aoqb@cin.ufpe.br>
2023-12-12 13:24:46 -0300

    feat: add library config

---
+++ src/utils/libraryConfig.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/ban-ts-comment */
+import { SearchConfig } from '@vtexdocs/components'
+
+const libraryConfig = {
+  appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '',
+  apiKey: process.env.NEXT_PUBLIC_ALGOLIA_WRITE_KEY || '',
+  index: 'helpcenter-docs',
+}
+
+export default SearchConfig(libraryConfig)
