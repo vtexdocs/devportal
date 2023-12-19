bebded52dbe9b4528caca10fe52fed1f40985431
Bianca Carneiro <bccnf@cin.ufpe.br>
2023-12-19 17:47:24 -0300

    feat: fb and ig icons

---
+++ src/components/icons/fb-icon.tsx
@@ -0,0 +1,34 @@
+import type { IconProps } from '@vtex/brand-ui'
+import { Icon } from '@vtex/brand-ui'
+
+const FbIcon = (props: IconProps) => (
+  <Icon
+    {...props}
+    viewBox="0 0 33 33"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M31.5501 16.8379C31.5501 25.3976 24.6 32.3379 16.025 32.3379C7.45006 32.3379 0.5 25.3976 0.5 16.8379C0.5 8.27821 7.45006 1.33789 16.025 1.33789C24.6 1.33789 31.5501 8.27821 31.5501 16.8379Z"
+      stroke="#4D596A"
+    />
+    <g clip-path="url(#clip0_311_37415)">
+      <path
+        d="M17.5268 17.9629H19.4047L20.1559 14.9629H17.5268V13.4629C17.5268 12.6904 17.5268 11.9629 19.0291 11.9629H20.1559V9.44289C19.911 9.41064 18.9863 9.33789 18.0098 9.33789C15.9703 9.33789 14.5221 10.5806 14.5221 12.8629V14.9629H12.2686V17.9629H14.5221V24.3379H17.5268V17.9629Z"
+        fill="#CCCED7"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_311_37415">
+        <rect
+          width="18.0282"
+          height="18"
+          fill="white"
+          transform="translate(7.01074 7.83789)"
+        />
+      </clipPath>
+    </defs>
+  </Icon>
+)
+
+export default FbIcon
