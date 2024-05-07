f9b631bb79d21d5d5649d22edb815ce58e137501
Carolina Menezes <carolinamenezes@hotmail.com.br>
2024-05-07 13:06:36 -0300

    fix: remove duplicated

--- src/utils/replaceHTMLBlocks.ts
+++ src/utils/replaceHTMLBlocks.ts
@@ -48,7 +48,6 @@ const replaceHTMLBlocks: (content: string) => string = (content) => {
   return content
     .replace(/<>/g, '\\<\\>')
     .replace(/(<br>\s*<\/br>)/g, '<br />')
-    .replace(/(<br>\s*<\/br>)/g, '<br />')
     .replace(/<img[^>]*?>/g, (match) => match.replace(/\/?>$/, ' />'))
     .replace(/<br>/g, '<br />')
     .replace(/<!--.*?-->/gs, '')
