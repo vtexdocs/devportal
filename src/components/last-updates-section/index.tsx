600d5fdc9cd3cf0cec68f24f63685f126aed2d35
Mariana Caetano Pereira <67270558+Mariana-Caetano@users.noreply.github.com>
2024-10-17 15:11:05 -0300

    fix(last-updates-section/index.tsx)

--- src/components/last-updates-section/index.tsx
+++ src/components/last-updates-section/index.tsx
@@ -10,7 +10,7 @@ const lastReleaseNote: CardProps = {
     type: 'added',
     description:
       'FastStore WebOps: Build logs now integrated into the app',
-    date: new Date('10/17/2024'),
+    date: new Date('10/17/2024'), 
   },
   updateType: 'release-notes',
 }
