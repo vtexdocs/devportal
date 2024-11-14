e761ee3f2930a9b792422ccbb9e7c068a4a35da0
Carolina Menezes <carolina.menezes@vtex.com.br>
2024-11-14 19:36:38 -0300

    feat(release-notes): add pagination

--- src/components/breadcrumb/index.tsx
+++ src/components/breadcrumb/index.tsx
@@ -1,4 +1,5 @@
 import { Link, Flex, IconCaret, Text } from '@vtex/brand-ui'
+import { useEffect, useRef, useState } from 'react'
 
 import styles from './styles'
 
@@ -7,31 +8,47 @@ interface Props {
 }
 
 const Breadcrumb = ({ breadcumbList }: Props) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isTruncated, setIsTruncated] = useState(false)
+  const breadcrumbRef = useRef<HTMLDivElement>(null)
+  const lastItemRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (breadcrumbRef.current && lastItemRef.current) {
+      const breadcrumbWidth = breadcrumbRef.current.offsetWidth
+      const lastItemWidth = lastItemRef.current.offsetWidth
+
+      setIsTruncated(breadcrumbWidth < lastItemWidth)
+    }
+  }, [breadcumbList])
+
   return (
-    <Flex sx={styles.breadcrumb}>
-      {breadcumbList.map((item, idx) =>
-        item.type === 'category' ? (
-          <>
-            <Text>{item.name}</Text>
-            {idx < breadcumbList.length - 1 ? (
-              <IconCaret direction="right" size={16} />
-            ) : (
-              ''
-            )}
-          </>
-        ) : (
-          <>
-            <Link sx={styles.breadcrumbItem} href={item.slug}>
+    <Flex sx={styles.breadcrumb} ref={breadcrumbRef}>
+      {breadcumbList.map((item, idx) => (
+        <>
+          {item.type === 'category' ? (
+            <Text
+              ref={idx === breadcumbList.length - 1 ? lastItemRef : undefined}
+              title={item.name}
+              sx={styles.breadcrumbItem}
+            >
+              {item.name}
+            </Text>
+          ) : (
+            <Link
+              ref={idx === breadcumbList.length - 1 ? lastItemRef : undefined}
+              sx={styles.breadcrumbItem}
+              href={item.slug}
+              title={item.name}
+            >
               {item.name}
             </Link>
-            {idx < breadcumbList.length - 1 ? (
-              <IconCaret direction="right" size={16} />
-            ) : (
-              ''
-            )}
-          </>
-        )
-      )}
+          )}
+          {idx < breadcumbList.length - 1 && (
+            <IconCaret direction="right" size={16} />
+          )}
+        </>
+      ))}
     </Flex>
   )
 }
