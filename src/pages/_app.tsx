56809a24fbe759f2c1398686e14ae51a598d973b
Bruno Amui <brunoamui@gmail.com>
2025-02-28 23:56:33 -0300

    fix(openreplay): remove openreplay tracker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,32 +15,29 @@ type Props = AppProps & {
   Component: Page
 }
 
-import TrackerProvider from 'utils/contexts/trackerContext'
 import PreviewContextProvider from 'utils/contexts/preview'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: Props) {
   return (
     <SessionProvider session={session}>
-      <TrackerProvider>
-        <Head>
-          <meta
-            property="og:image"
-            content="https://cdn.jsdelivr.net/gh/vtexdocs/devportal@main/public/images/meta-image.png"
-          />
-        </Head>
-        <PreviewContextProvider>
-          <Layout
-            sidebarfallback={pageProps.sidebarfallback}
-            hideSidebar={Component.hideSidebar}
-            isPreview={pageProps.isPreview ?? false}
-            isEditor={Component.isEditor}
-            sectionSelected={pageProps.sectionSelected}
-            parentsArray={pageProps.parentsArray}
-          >
-            <Component {...pageProps} />
-          </Layout>
-        </PreviewContextProvider>
-      </TrackerProvider>
+      <Head>
+        <meta
+          property="og:image"
+          content="https://cdn.jsdelivr.net/gh/vtexdocs/devportal@main/public/images/meta-image.png"
+        />
+      </Head>
+      <PreviewContextProvider>
+        <Layout
+          sidebarfallback={pageProps.sidebarfallback}
+          hideSidebar={Component.hideSidebar}
+          isPreview={pageProps.isPreview ?? false}
+          isEditor={Component.isEditor}
+          sectionSelected={pageProps.sectionSelected}
+          parentsArray={pageProps.parentsArray}
+        >
+          <Component {...pageProps} />
+        </Layout>
+      </PreviewContextProvider>
     </SessionProvider>
   )
 }
