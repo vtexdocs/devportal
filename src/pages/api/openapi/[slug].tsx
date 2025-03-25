6ce1546b7b0213cf8f93befc9b0b66276dc8e53e
Karen Krieger <karen.krieger@vtex.com.br>
2025-03-25 12:16:36 -0300

    feat: add Operational Capacity API to navigation

--- src/pages/api/openapi/[slug].tsx
+++ src/pages/api/openapi/[slug].tsx
@@ -33,6 +33,7 @@ const referencePaths = objectFlip({
   'VTEX - Master Data API - v2': 'master-data-api-v2',
   'VTEX - MasterData API - v10.2': 'masterdata-api',
   'VTEX - Message Center API': 'message-center-api',
+  'VTEX - Operational Capacity API': 'operational-capacity-api',
   'VTEX - Orders API PII version': 'orders-api-pii-version',
   'VTEX - Orders API': 'orders-api',
   'VTEX - Payment Provider Protocol': 'payment-provider-protocol',
