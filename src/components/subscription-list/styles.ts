1a4d40663ddb34112f33cbe5cc5dbec133d3039d
mariana-caetano <mariana.caetano@vtex.com>
2025-05-13 16:06:30 -0300

    styles(styles): fix texts

--- src/components/subscription-list/styles.ts
+++ src/components/subscription-list/styles.ts
@@ -32,7 +32,7 @@ const title: SxStyleProp = {
 }
 
 const description: SxStyleProp = {
-  fontSize: ['12px', '16px'],
+  fontSize: ['14px', '16px'],
   lineHeight: ['16px', '22px'],
   textAlign: 'center',
   marginBottom: '16px',
@@ -62,7 +62,7 @@ const input: SxStyleProp = {
 }
 
 const privacyText: SxStyleProp = {
-  fontSize: ['8px', '12px'],
+  fontSize: ['11px', '14px'],
   lineHeight: ['16px', '22px'],
   pl: ['22px', '32px'],
   fontWeight: '400',
@@ -75,13 +75,6 @@ const message = (isSuccess: boolean): SxStyleProp => ({
   borderRadius: '5px',
 })
 
-const messageCard: SxStyleProp = {
-  backgroundColor: 'white',
-  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-  padding: '10px',
-  borderRadius: '8px',
-}
-
 const popupCard: SxStyleProp = {
   backgroundColor: 'white',
   boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
@@ -93,7 +86,7 @@ const popupCard: SxStyleProp = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  borderRadius: '8px',
+  borderRadius: '6px',
 }
 
 const globalStyles: SxStyleProp = {
@@ -113,7 +106,6 @@ export default {
   input,
   privacyText,
   message,
-  messageCard,
   popupCard,
   globalStyles,
 }
