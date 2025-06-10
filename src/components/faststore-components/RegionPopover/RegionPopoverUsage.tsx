b87bc08d66a6f1edf547365a686e59c5fe1fa390
Bárbara Celi <112641072+barbara-celi@users.noreply.github.com>
2025-06-10 13:36:26 -0300

    feat(faststore-components): creating RegionPopoverUsage.tsx

---
+++ src/components/faststore-components/RegionPopover/RegionPopoverUsage.tsx
@@ -0,0 +1,36 @@
+import { Button, RegionPopover } from '@faststore/ui'
+import { useRef } from 'react'
+
+function RegionPopoverUsage() {
+  const popoverTrigger = useRef(null)
+
+  return (
+    <section>
+      <Button
+        ref={popoverTrigger}
+        variant="primary"
+          >
+        Change region</Button>
+      <RegionPopover
+        triggerRef={popoverTrigger}
+        title="Update Location"
+        closeButtonAriaLabel="Close"
+        inputField={{
+          label: "Enter your postal code",
+          buttonActionText: "Save",
+        }}
+        textBeforeLocation="Current region:"
+        textAfterLocation="(You can change this anytime below.)"
+        description="Please provide your region to continue."
+        placement="bottom-center"
+        offsetTop={12}
+        offsetLeft={8}
+        onDismiss={() => {
+          alert('RegionPopover dismissed')
+        }}
+      />
+    </section>
+  )
+}
+
+export default RegionPopoverUsage
