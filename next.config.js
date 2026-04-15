cea21b5d983e1f233a0d7e4dc461841d13a8877c
Pedro Antunes <47991446+PedroAntunesCosta@users.noreply.github.com>
2026-04-15 14:48:42 -0300

    fix: patch Lit SSR path normalization for Windows builds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,59 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const { withPlaiceholder } = require('@plaiceholder/next')
-const withLitSSR = require('@lit-labs/nextjs')()
+
+const withPatchedLitSSR = (baseConfig = {}) => ({
+  ...baseConfig,
+  webpack: (config, options) => {
+    const { isServer, nextRuntime, webpack } = options
+
+    config.module.rules.unshift({
+      test: /\/pages\/.*\.(?:jsx?|tsx?)$/,
+      exclude: /next\/dist\//,
+      loader: 'imports-loader',
+      options: {
+        imports: ['side-effects @lit-labs/ssr-react/enable-lit-ssr.js'],
+      },
+    })
+
+    if (isServer && nextRuntime === 'nodejs') {
+      const nextHandleExternals = config.externals[0]
+      config.externals = [
+        (opt) => {
+          if (
+            opt.request === 'react/jsx-dev-runtime' ||
+            opt.request === 'react/jsx-runtime'
+          ) {
+            return Promise.resolve()
+          }
+
+          return nextHandleExternals(opt)
+        },
+      ]
+    }
+
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/react/, (resource) => {
+        const normalizedContext = (resource.context || '').replace(/\\/g, '/')
+        const isLitSsrRuntime = /labs\/ssr-react/.test(normalizedContext)
+
+        if (resource.request === 'react/jsx-runtime' && !isLitSsrRuntime) {
+          resource.request = '@lit-labs/ssr-react/jsx-runtime'
+        }
+
+        if (resource.request === 'react/jsx-dev-runtime' && !isLitSsrRuntime) {
+          resource.request = '@lit-labs/ssr-react/jsx-dev-runtime'
+        }
+      })
+    )
+
+    if (typeof baseConfig.webpack === 'function') {
+      return baseConfig.webpack(config, options)
+    }
+
+    return config
+  },
+})
 
 const nextConfig = {
   experimental: {
@@ -55,7 +107,7 @@ const nextConfig = {
 }
 
 module.exports = () => {
-  const plugins = [withPlaiceholder, withLitSSR]
+  const plugins = [withPlaiceholder, withPatchedLitSSR]
   return plugins.reduce((acc, plugin) => plugin(acc), {
     ...nextConfig,
   })
