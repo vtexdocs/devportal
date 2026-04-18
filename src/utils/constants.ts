25b4c882cf9cbb70105a61bb0e2000000db3d8cd
Pedro Antunes <47991446+PedroAntunesCosta@users.noreply.github.com>
2026-04-17 22:34:54 -0300

    feat: align B2B buyer data and addresses API navigation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -497,6 +497,6 @@ export const openapiMappings: { [key: string]: string } = {
   'VTEX - Custom Fields API': 'custom-fields-api',
   'VTEX - Default Values API': 'default-values-api',
   'VTEX - B2B Buyer Data API': 'b2b-buyer-data-api',
-  'VTEX - B2B Recipients API': 'b2b-recipients-api',
+  'VTEX - B2B Addresses API': 'b2b-addresses-api',
   'VTEX - B2B Contracts API': 'b2b-contracts-api',
 }
