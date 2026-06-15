ba904bea1b654f980d1981fc4f3efa298464b0c2
julia-rabello <77292838+julia-rabello@users.noreply.github.com>
2026-06-15 15:40:33 -0300

    chore: add B2B Password Migration Protocol API reference and navigation entries

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -500,5 +500,6 @@ export const openapiMappings: { [key: string]: string } = {
   'VTEX - B2B Buyer Data API': 'b2b-buyer-data-api',
   'VTEX - B2B Addresses API': 'b2b-addresses',
   'VTEX - B2B Contracts API': 'b2b-contracts-api',
+  'VTEX - B2B Password Migration Protocol': 'b2b-password-migration-protocol',
   'VTEX - User Rights API': 'user-rights-api',
 }
